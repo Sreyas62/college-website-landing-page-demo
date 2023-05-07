c4e4d7428fbfc345d50f287e6429290bf77d05ab
Sreyas M Pillai <96684846+Sreyas62@users.noreply.github.com>
2023-05-08 01:24:28 +0530

    animation added

--- src/app/components/Contact/Contact.js
+++ src/app/components/Contact/Contact.js
@@ -2,10 +2,17 @@
 import "./styles.css";
 import {MdLocationOn,MdEmail} from "react-icons/md";
 import {IoMdContact} from "react-icons/io";
-import { useRef } from 'react';
+import { useRef,useEffect } from 'react';
 import emailjs from '@emailjs/browser';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Contact() {
+    useEffect(() => {
+        AOS.init({
+          duration: 800,
+        });
+      }, []);
     const form = useRef();
 
   const sendEmail = (e) => {
@@ -20,15 +27,13 @@ function Contact() {
       e.target.reset();
   };
   return (
-    <div className="sec">
-        {/* <section class="location">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.2002179315555!2d77.27999911492367!3d19.18645548702458!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd1d7f29ec9d4b5%3A0x87c4833f7786ba8b!2sAvitulya%20Nivas!5e0!3m2!1sen!2sin!4v1677829762454!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </section> */}
+    <div className="sec" data-aos="fade-up">
+
     
     <section className="contact-us">
         <h1 className="ccc">Contact us</h1>
         <div className="row">
-            <div className="contact-col">
+            <div className="contact-col" data-aos="fade-right">
                 <div>
                     <MdLocationOn className="ic" size="2.5em"/>               
                     <span>
@@ -54,7 +59,7 @@ function Contact() {
                     </span>
                 </div>
             </div>
-            <div className="contact-col">
+            <div className="contact-col" data-aos="fade-left">
                 <form action="submit" ref={form} onSubmit={sendEmail}>
                     <input className="ff" type="text" name="user_name" placeholder="Enter your name" required/>
                     <input type="email" name="user_email" placeholder="Enter email address" required/>
@@ -65,9 +70,9 @@ function Contact() {
             </div>
         </div>
     </section>
-    <h1 className="mm">Locate us</h1>
-    <div className="map">
     
+    <div className="map" data-aos="fade-up">
+    <h1 className="mm" data-aos="fade-up">Locate us</h1>
     <iframe
       title="Google Maps"
       src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7874.354761650065!2d76.617834!3d9.317546!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0622ea027eb08f%3A0x41105b207db821c6!2sCollege%20of%20Engineering%20Chengannur!5e0!3m2!1sen!2sus!4v1683437991768!5m2!1sen!2sus"
