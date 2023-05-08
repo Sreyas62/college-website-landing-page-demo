dd79b312c3eb1dfa5379b3c9b6f2917972e035b3
Sreyas M Pillai <96684846+Sreyas62@users.noreply.github.com>
2023-05-08 16:26:28 +0530

    notifications and events

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -2,6 +2,7 @@ import './styles.css';
 import Link from 'next/link';
 function Footer() {
   return (
+    
     <div className='foot'>Developed by &nbsp; <Link href={"http://www.linkedin.com/in/sreyas-m-pillai/"}><button className='bb'><h6 className='name'>Sreyas M Pillai</h6></button></Link></div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
-import SideBar from "./components/NavBar/NavBar"
+import NavBar from "./components/NavBar/NavBar"
 import Hero from "./components/Hero/Hero"
 import Contact from "./components/Contact/Contact"
 import Footer from "./components/Footer/Footer"
+import Notification from "./components/Notification/Notification"
 function page() {
   return (
     <>
-    <SideBar/>
+    <NavBar/>
     <Hero/>
+    <Notification/>
     <Contact />
     <Footer/>
     </>
